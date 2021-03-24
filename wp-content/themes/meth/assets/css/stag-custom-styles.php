e5ebe367e2c14215869789f5cc78955ef2b3d7f4
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2021-03-24 19:12:37 +0800

    Add files via upload

---
+++ wp-content/themes/meth/assets/css/stag-custom-styles.php
@@ -0,0 +1,122 @@
+@media (max-width: 480px) { 
+.stag_widget_intro
+  .stag_widget_intro .intro-description {
+    font-size: 20px; }
+  .stag_widget_intro .widgettitle {
+    font-size: 45px; }
+  .stag_widget_static_content-2 .comment-content h4, .entry-content h4, .textwidget h4 {
+    font-size: 20px; }
+
+.features-widget .has-image .feature-image,
+.features-widget .has-image .feature-content {
+  display: block;
+  vertical-align: top;
+  margin-top: 8%; 
+  margin-right: 0em; }
+.features-widget .has-image .feature-image {
+  width: 100%;
+  height: 100%;
+  padding-left: 0px; }
+/* .features-widget .has-image .feature-image img {
+  height: 50%;
+  width: 50%; } */
+.features-widget .has-image .feature-content {
+  width: 100%;
+  margin-bottom: 25%;
+  padding-left: 0%; }
+
+.feature-widget-4 .feature-image {
+  margin-bottom: 50px;
+  margin-bottom: 3.125rem; }
+.feature-widget-4 .features-widget .feature-image,
+.feature-widget-4 .features-widget .feature-content {
+  display: block;
+  width: 100%; }
+}
+
+.contact-form input[type=email], .contact-form input[type=text], .contact-form textarea { width: 100% !important; } 
+
+.post-modal .portfolio-header .entry-title, .single-portfolio .portfolio-header .entry-title {
+  color: #000; 
+}
+
+/* .display-intro .site-title { font-family: 'Pacifico', cursive !important; } */
+
+/* Theme Settings */
+body,
+.stag-custom-widget-area .widgettitle span,
+.comments-title span,
+#cancel-comment-reply-link,
+.comment-reply-title span {
+	background-color: #ffffff;
+}
+
+.portfolio-grid .portfolio,
+.stag_widget_team .team {
+	border-color: #ffffff;
+}
+
+
+button,
+.button,
+input[type='reset'],
+input[type='submit'],
+input[type='button'],
+.blog-nav a,
+.page-numbers,
+#nprogress .bar,
+.blog .page-header,
+.accent-background,
+.search .page-header,
+.archive .page-header,
+.tax-skill .entry-header,
+.blog .page-header,
+.error404 .page-header,
+.search .page-header,
+.archive .page-header,
+.stag-tabs--normal .stag-nav a,
+.stag-toggle--normal .stag-toggle-title,
+.page-template-default.page .entry-header,
+.ui-accordion-header-active.stag-toggle-title,
+.page-template-template-archive-php .entry-header,
+.page-template-template-contact-php .entry-header,
+.page-template-template-portfolio-php .entry-header {
+	background-color: #ff5050;
+}
+
+#nprogress .peg {
+	-webkit-box-shadow: 0 0 10px #ff5050, 0 0 5px #ff5050;
+	-moz-box-shadow: 0 0 10px #ff5050, 0 0 5px #ff5050;
+	box-shadow: 0 0 10px #ff5050, 0 0 5px #ff5050;
+}
+
+a,
+.accent-color,
+.custom-widgets-section li,
+.portfolio-filter a.active,
+.custom-widgets-section a:hover {
+	color: #ff5050;
+}
+
+.stag-icon-link:hover,
+.stag-social-icons a:hover {
+	color: #ff5050 !important;
+}
+
+@media (min-width: 900px) {
+	.main-navigation ul ul a:hover {
+		background-color: #ff5050 !important;
+	}
+}
+body { font-family: 'Roboto'; }
+
+blockquote,
+.entry-meta,
+.post-navigation,
+.paging-navigation,
+.portfolio-filter a,
+h1, h2, h3, h4, h5, h6,
+.stag_widget_blog time,
+.comment-author .fn {
+	font-family: 'Roboto Slab';
+}
